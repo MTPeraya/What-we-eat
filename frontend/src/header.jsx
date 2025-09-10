1802f607321e680079d05fb91cd196effc4f9b95
LylatierPim <nattanan.pim@ku.th>
2025-09-10 15:31:28 +0700

    Ant: add the header element

---
+++ frontend/src/header.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './App.css'
+
+function Profile(){
+
+    return <div className='profile-s Margin1vh'></div>
+}
+
+function MenuIcon(){
+  return(
+  <svg className="Margin1vh" width="8vh" height="8vh" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <path d="M4 6H20M4 12H20M4 18H20" stroke="white" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+  </svg>
+  )
+}
+
+function Header() {
+
+  return (
+    <div className='header'>
+        <a><MenuIcon /></a>
+        <a><Profile/></a>
+    </div>
+  )
+}
+
+
+export default Header
