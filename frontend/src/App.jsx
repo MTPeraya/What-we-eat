7ed061e7911eda0768e9a7a739a0e3aa4dcc4665
LylatierPim <nattanan.pim@ku.th>
2025-09-16 21:23:19 +0700

    Ant: Make a homepage

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,35 @@
 import { useEffect, useState } from "react";
+
+// import Header from './header.jsx'
+// import HeroSection from './components/hero.jsx'
+// import Features from './components/feature.jsx'
+// import Footer from './components/bigfooter.jsx'
+
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
+import HomePage from "./Homepage";
 
 function App() {
-  const [message, setMessage] = useState("Loading...");
+  // const [message, setMessage] = useState("Loading...");
 
-  useEffect(() => {
-    // Call backend API
-    fetch("http://localhost:4001/api/hello") 
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => {
-        console.error("Error fetching backend:", err);
-        setMessage("Error: could not connect to backend");
-      });
-  }, []);
+  // useEffect(() => {
+  //   // Call backend API
+  //   fetch("http://localhost:4001/api/hello") 
+  //     .then((res) => res.json())
+  //     .then((data) => setMessage(data.message))
+  //     .catch((err) => {
+  //       console.error("Error fetching backend:", err);
+  //       setMessage("Error: could not connect to backend");
+  //     });
+  // }, []);
 
   return (
-    <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-      <h1>Frontend is running ✅</h1>
-      <p>Backend says: <strong>{message}</strong></p>
+    // <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
+    //   <h1>Frontend is running ✅</h1>
+    //   <p>Backend says: <strong>{message}</strong></p>
+    // </div>
+    <div>
+      <HomePage/>
     </div>
   );
 }
