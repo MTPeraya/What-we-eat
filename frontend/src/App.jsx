81fa137a54a5a9a8cac8832a5e89e69586a85b11
LylatierPim <nattanan.pim@ku.th>
2025-09-16 21:02:28 +0700

    Ant: downgrade to react18 and install bootstrap

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
 import { useEffect, useState } from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css'
 
 function App() {
   const [message, setMessage] = useState("Loading...");
