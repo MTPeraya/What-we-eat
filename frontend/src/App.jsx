5b95d9e7fc221cb8abbddccef3a8091878db7cb9
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2025-10-05 03:16:51 +0700

    update App.css and App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,29 +10,8 @@ import './App.css'
 import HomePage from "./Homepage";
 
 function App() {
-  // const [message, setMessage] = useState("Loading...");
-
-  // useEffect(() => {
-  //   // Call backend API
-  //   fetch("http://localhost:4001/api/hello") 
-  //     .then((res) => res.json())
-  //     .then((data) => setMessage(data.message))
-  //     .catch((err) => {
-  //       console.error("Error fetching backend:", err);
-  //       setMessage("Error: could not connect to backend");
-  //     });
-  // }, []);
 
   return (
-<<<<<<< Updated upstream
-    // <div style={{ fontFamily: "Arial", textAlign: "center", marginTop: "50px" }}>
-    //   <h1>Frontend is running ✅</h1>
-    //   <p>Backend says: <strong>{message}</strong></p>
-    // </div>
-    <div>
-      <HomePage/>
-    </div>
-=======
     // <div>
     //   <HomePage/>
     //   {/* <Register/> */}
@@ -50,7 +29,6 @@ function App() {
         <Route path="/create-room" element={<CreateRoom />} />
       </Routes>
     </Router>
->>>>>>> Stashed changes
   );
 }
 
