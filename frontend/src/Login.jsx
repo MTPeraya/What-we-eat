ae0dbddeadb941d3944454f8aeeb3cbf13639abc
Chanunchida Dithakumarn <chanunchida.d@ku.th>
2025-10-04 21:01:59 +0700

    Ming: push update register page

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -4,6 +4,7 @@ import { Link, useNavigate } from "react-router-dom";
 
 // remaining : check exist username, is username match with password
 
+
 function Login() {
     const navigate = useNavigate();
 
