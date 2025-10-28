673098c044fb306d8a21bbb966d05e39991462ad
LylatierPim <nattanan.pim@ku.th>
2025-10-28 21:47:38 +0700

    UserProfile: mergeing

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -9,8 +9,5 @@ import UserPage from './UserPage.jsx'
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
-    {/* <Result/> */}
-    {/* <FoodTinder/> */}
-    {/* <DrawPage/> */}
   </StrictMode>,
 )
